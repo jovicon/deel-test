dd5ad389ae4b3a8ff98181ad97c1583f032e01ea
José Contreras <jocontrera16@gmail.com>
2024-03-17 18:33:20 -0300

    fixed: '/contracts'

--- src/app.js
+++ src/app.js
@@ -14,8 +14,11 @@ app.set("models", sequelize.models);
 app.get("/contracts/:id", getProfile, async (req, res) => {
   const { Contract } = req.app.get("models");
   const { id } = req.params;
+  const { profile } = req;
 
-  const contract = await Contract.findOne({ where: { id } });
+  const contract = await Contract.findOne({
+    where: { id, ClientId: profile.id },
+  });
 
   if (!contract) return res.status(404).end();
 
