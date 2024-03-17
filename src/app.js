0ff082b04881a2f3b58f5b5b3030ccc79fa767c4
José Contreras <jocontrera16@gmail.com>
2024-03-17 18:23:55 -0300

    fixed: formatting

--- src/app.js
+++ src/app.js
@@ -14,8 +14,12 @@ app.set("models", sequelize.models);
 app.get("/contracts/:id", getProfile, async (req, res) => {
   const { Contract } = req.app.get("models");
   const { id } = req.params;
+
   const contract = await Contract.findOne({ where: { id } });
+
   if (!contract) return res.status(404).end();
+
   res.json(contract);
 });
+
 module.exports = app;
